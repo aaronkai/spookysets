b63844b3941afe7d88e79623638df415cdc6bb27
aaron <aaron.kai.hubbard@gmail.com>
2021-10-28 10:29:53 -0400

    added sex to bodyfat calculator

--- src/routes/pages/bodyfat.svelte
+++ src/routes/pages/bodyfat.svelte
@@ -3,47 +3,72 @@
 </script>
 
 <script type="ts">
+	enum Sex {
+		Male = 'MALE',
+		Female = 'FEMALE',
+	}
+
 	let lbs: number;
 	let neck: number;
-	let abdomen: number;
+	let waist: number;
 	let height: number;
 	let bodyfatPercentage: number;
+	let sex: Sex = 'MALE';
+	let hips: number = 0;
 
-	function processForm(lbs: number, neck: number, abdomen: number, height: number): number {
-		bodyfatPercentage =
-			Math.round((86.01 * Math.log10(abdomen - neck) - 70.041 * Math.log10(height) + 36.76) * 100) /
-			100;
+	function processForm(lbs: number, neck: number, waist: number, height: number, hips): number {
+		if (sex === Sex.Male) {
+			bodyfatPercentage =
+				Math.round((86.01 * Math.log10(waist - neck) - 70.041 * Math.log10(height) + 36.76) * 100) /
+				100;
+		} else {
+			bodyfatPercentage =
+				Math.round(
+					(163.205 * Math.log10(waist + hips - neck) - 97.684 * Math.log10(height) - 78.387) * 100,
+				) / 100;
+		}
 		return bodyfatPercentage;
 	}
 
 	function handleReset(): void {
 		bodyfatPercentage = undefined;
 	}
+
+	// 	formula for men
+	//  86.010 x log10 (waist - neck) - 70.041 x log10 (height) + 36.76
+	//  formula for women
+	//  163.205 x log10 (waist + hip - neck) - 97.684 x log10 (height) - 78.387
 </script>
 
-<!-- formula for men -->
-<!--  86.010 x log10 (abdomen - neck) - 70.041 x log10 (height) + 36.76 -->
-
-<!-- To Do -->
-<!-- Add button for sex -->
-<!--  163.205 x log10 (waist + hip - neck) - 97.684 x log10 (height) - 78.387 -->
-<!-- on submit, replace form with result with transition -->
 <main>
 	<header>
 		<h1>Bodyfat Calculator</h1>
 		<h2>Navy-seal Formula</h2>
 	</header>
 	{#if !bodyfatPercentage}
-		<form on:submit|preventDefault={processForm(lbs, neck, abdomen, height)}>
+		<form on:submit|preventDefault={processForm(lbs, neck, waist, height, hips)}>
 			<h3>Your Biometrics:</h3>
+
+			<label for="male">
+				<input type="radio" id="male" name="sex" bind:group={sex} value={Sex.Male} />
+				{Sex.Male}
+			</label>
+			<label for="female">
+				<input type="radio" id="female" name="sex" bind:group={sex} value={Sex.Female} />
+				{Sex.Female}
+			</label>
 			<label for="lbs">Weight in pounds:</label>
 			<input required type="number" step="0.01" bind:value={lbs} id="lbs" name="lbs" />
 			<label for="neck">Neck in inches:</label>
 			<input required type="number" step="0.01" bind:value={neck} id="neck" name="neck" />
-			<label for="abdomen">Abdomen at belly-button in inches:</label>
-			<input required type="number" step="0.01" bind:value={abdomen} id="abdomen" name="abdomen" />
+			<label for="waist">Waist at belly-button in inches:</label>
+			<input required type="number" step="0.01" bind:value={waist} id="waist" name="waist" />
 			<label for="height">Height in inches:</label>
 			<input required type="number" step="0.01" bind:value={height} id="height" name="height" />
+			{#if sex === Sex.Female}
+				<label for="height">Hips in inches:</label>
+				<input required type="number" step="0.01" bind:value={hips} id="height" name="height" />
+			{/if}
 			<button type="submit">Calculate Bodyfat Percentage</button>
 		</form>
 	{:else}
@@ -78,6 +103,7 @@
 		border: var(--border-size) solid var(--purple);
 		border-radius: var(--rounded-lg);
 		display: grid;
+		grid-template-columns: 1fr 1fr;
 		row-gap: var(--spacing-md);
 		column-gap: var(--spacing-xs);
 		max-width: 500px;
@@ -90,8 +116,10 @@
 		font-size: var(--font-lg);
 		text-align: center;
 	}
+	form div {
+		grid-column: span 2;
+	}
 	label {
-		/* margin: 1rem 0; */
 		font-family: bungee;
 		font-size: var(--font-sm);
 	}
